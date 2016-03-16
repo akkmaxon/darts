f42e3332e4a1a02760bb866730b80770f415c467
Akkuzin Maxim <akkmaxon2307@gmail.com>
2016-03-16 15:20:37 +0300

    Added background and links on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-    root 'darts#home'
-    get 'video_online' => 'darts#video_online'
-    get 'competitions' => 'darts#competitions'
-    get 'resources' => 'darts#resources'
-    get 'contacts' => 'darts#contacts'
+  root 'darts#home'
+  get 'video_online' => 'darts#video_online'
+  get 'competitions' => 'darts#competitions'
+  get 'resources' => 'darts#resources'
+  get 'contacts' => 'darts#contacts'
 end
