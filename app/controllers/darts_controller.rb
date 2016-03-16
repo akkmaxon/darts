a70853dc9bb3c9c0c99cdcb61e65ef969fb91833
Akkuzin Maxim <akkmaxon2307@gmail.com>
2016-03-16 12:42:57 +0300

    First steps

---
+++ app/controllers/darts_controller.rb
@@ -0,0 +1,17 @@
+class DartsController < ApplicationController
+
+  def home
+  end
+
+  def video_online
+  end
+
+  def competitions
+  end
+
+  def resources
+  end
+
+  def contacts
+  end
+end
---
+++ test/controllers/darts_controller_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class DartsControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "a[href=?]", video_online_path
+    assert_select "a[href=?]", competitions_path
+    assert_select "a[href=?]", resources_path
+    assert_select "a[href=?]", contacts_path
+    assert_select 'header', count: 0
+  end
+
+  test "should get video_online" do
+    get :video_online
+    assert_response :success
+  end
+  
+  test "should get competitions" do
+    get :competitions
+    assert_response :success
+  end
+
+  test "should get resources" do
+    get :resources
+    assert_response :success
+  end
+
+  test "should get contacts" do
+    get :contacts
+    assert_response :success
+  end
+end
